5ad41b310761a0b14d1a1bc889e7470727129b43
Guillaume Guerra <guillaume.guerra@younited-credit.fr>
2020-01-11 19:00:07 +0100

    wip

--- NasRightsManager/Program.cs
+++ NasRightsManager/Program.cs
@@ -1,29 +1,53 @@
 ﻿using System;
 using System.Collections;
 using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
 using System.Security.Principal;
 
 namespace NasRightsManager
 {
     class Program
     {
-        public static string[] Directories => new[] { "Animation", "Films", "Series" };
+        public static string[] Directories => new[] { "test" };
+        //public static string[] Directories => new[] { "Animation", "Films", "Series" };
+
+        private static readonly SecurityIdentifier Everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
         // CF : https://stackoverflow.com/questions/40449973/how-to-modify-file-access-control-in-net-core
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+
             foreach (var directory in Directories)
             {
                 Console.WriteLine($"Inspecting directory {directory}");
-                foreach (var file in Directory.GetFiles($@"Z:\{directory}","*.*",SearchOption.AllDirectories))
+
+                foreach (var file in Directory.GetFiles($@"Z:\{directory}", "*.*", SearchOption.AllDirectories))
                 {
-                    var descriptor=new FileInfo(Path.GetFullPath(file));
+                    var fullPath = Path.GetFullPath(file);
+
+                    var descriptor = new FileInfo(fullPath);
                     var accessControl = descriptor.GetAccessControl();
-                    var authorization = accessControl.GetAccessRules(true,true, typeof(NTAccount));
+                    var authorization = accessControl.GetAccessRules(true, true, typeof(NTAccount));
+                    if (!CheckRights(authorization))
+                    {
+                        Console.WriteLine($"Patching file {fullPath}");
+                        //accessControl.AddAccessRule(new FileSystemAccessRule(Everyone, FileSystemRights.FullControl, AccessControlType.Allow));
+                        //descriptor.SetAccessControl(accessControl);
+                    }
+
+                    //authorization.AddRule(
                 }
             }
         }
 
+        private static bool CheckRights(AuthorizationRuleCollection authorization)
+        {
+            return authorization.Cast<AccessRule>()
+                .Where(r => r is FileSystemAccessRule)
+                .Cast<FileSystemAccessRule>()
+                .Any(r => r.IdentityReference.Value == Everyone.Value && r.FileSystemRights == FileSystemRights.FullControl);
+        }
     }
 }
